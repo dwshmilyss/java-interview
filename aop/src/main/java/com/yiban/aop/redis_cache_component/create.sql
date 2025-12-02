e636d4133231c696871a025f0b3b7281414a9c56
weiwei.duan <weiwei.duan@linkflowtech.com>
2025-12-02 16:50:12 +0800

    利用redis做缓存组件 first commit

---
+++ aop/src/main/java/com/yiban/aop/redis_cache_component/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `t_user`(
+`id` int unsigned NOT NULL AUTO_INCREMENT,
+`username` varchar(50) NOT NULL DEFAULT '' COMMENT '用户名',
+`password` varchar(50) NOT NULL DEFAULT '' COMMENT '密码',
+`sex` tinyint NOT NULL DEFAULT '0' COMMENT '性别 0=女1=男',
+`deleted` tinyint unsigned NOT NULL DEFAULT '0' COMMENT '删除标志，默认0不删除，1删除',
+`update time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+`create time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+PRIMARY KEY(`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='用户表';
